402ef86845f35e7e81831a6c27f242a501fb7670
prio101 <miprio101@gmail.com>
2021-10-14 15:39:46 +0000

    feature: added the api for the quizes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  scope 'api' do
+    scope 'v1' do
+      resources :quizes, only: %i( index create show destroy )
+    end
+  end
+  
 end
