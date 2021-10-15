77f806541e41e1bbb56bce2bfd8cd528fb4b09be
prio101 <miprio101@gmail.com>
2021-10-15 10:47:39 +0000

    spec: quiz model

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Quiz, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:quiz_invalid) { FactoryBot.build(:quiz, 
+                                  topic: '', 
+                                  questions_count: nil, 
+                                  correct_answer_reward: nil) }
+  let(:quiz) { FactoryBot.build(:quiz, 
+                                  topic: 'topicA')}
+  
+  let(:quiz_two) { FactoryBot.build(:quiz, 
+                                      topic: 'topicA') }                              
+  describe 'validations' do
+    it 'should be invalid without topic' do
+      expect(quiz_invalid).to be_invalid
+    end
+    
+    it 'should be unique topic' do
+      quiz
+      expect(quiz_two).not_to be_valid
+    end
+
+    it 'should be invalid without question count' do
+      expect(quiz_invalid).not_to be_valid
+    end
+
+    it 'should be invalid without correct answer reward' do
+      expect(quiz_invalid).not_to be_valid
+    end
+  end
 end
