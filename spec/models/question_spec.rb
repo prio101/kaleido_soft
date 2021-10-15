81f1e64a182b135dabbd6d9c48bbfbd15fd073a9
prio101 <miprio101@gmail.com>
2021-10-15 11:08:18 +0000

    spec: question spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:quiz) { FactoryBot.create(:quiz) }
+  let(:question) { FactoryBot.create(:question, quiz: quiz) }
+  describe 'should belongs to quiz' do
+    it 'should have the quiz association' do
+      expect(question.quiz_id).to eq quiz.id
+    end
+  end
 end
