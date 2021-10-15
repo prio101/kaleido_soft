84ce18f063a4d3a0836b33904fa31e5065ef9a05
prio101 <miprio101@gmail.com>
2021-10-15 21:58:54 +0000

    fix: valid limit error

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,8 @@ class Question < ApplicationRecord
   belongs_to :quiz
 
   def valid_limit?
-    count = self.where(quiz_id: self.quiz_id).count
-    limit = Quiz.find(self.quiz_id).questions_count
+    count = Question.where(quiz_id: quiz_id).count
+    limit = Quiz.find(quiz_id).questions_count
     count < limit
   end
 end
