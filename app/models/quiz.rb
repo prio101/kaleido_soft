977e59289dc9b21a038f404b442a81bc26e4fe17
prio101 <miprio101@gmail.com>
2021-10-15 14:26:32 +0000

    feature: added difficulty

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,8 +1,9 @@
 class Quiz < ApplicationRecord
   has_many  :questions
   
+  enum difficulty: { easy: 0, hard: 1, hardest: 2 } 
 
   # validations
-  validates_presence_of :topic, :questions_count, :correct_answer_reward
+  validates_presence_of :topic, :questions_count, :correct_answer_reward, :difficulty
   validates_uniqueness_of  :topic
 end
